3d7551c405d98fc70a25814a712ea296b9cd85f5
RamonRomeroTec <A01700318@itesm.mx>
2018-04-12 22:23:43 -0500

    Inicial Settings 2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ownJodido
 Python 3.6.5
 pip-9.0.2
+
+pip install psycopg2-binary"
+pip install psycopg2
---
+++ src/db_sc.sh
@@ -0,0 +1,38 @@
+echo "Drop USER jodido;CREATE USER jodido;Drop DATABASE jodido;CREATE DATABASE jodido OWNER jodido;" > new.sql
+
+
+psql<new.sql
+
+rm -rf ./new.sql
+
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+echo ''
+echo 'Borrado de creación de migraciones'
+echo ''
+
+python3 manage.py makemigrations
+
+echo ''
+echo 'Ejecutando de migraciones '
+echo ''
+
+
+python3 manage.py migrate
+
+
+echo ''
+echo '>>>>>>Creacion de superusuario U:jodido  P:jodido '
+echo ''
+python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('jodido', 'jodido@example.com', '0123456abc')"
+
+
+
+echo ''
+echo 'Corriendo servidor ...'
+echo ''
+
+python3 manage.py runserver
+
+
+
