3ddd9d2177a66cb73ca5ceafa7bcc224f0233cf9
Ale Lopez <a01208598@itesm.mx>
2018-04-29 23:06:07 -0500

    generatorAle Crea txt de los lugares a 10km

---
+++ python/generatorAle.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import time
+
+GMAPS_API_KEY = 'AIzaSyCXm58tMXQ48sO1IKP956SRE-hrwswn1GQ'
+
+class startpoint():
+#https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=<latitude>,<longitude>&radius=<radio>&type=<type>&key=<GMAPS_API_KEY
+    latitude = 20.5923144
+    longitude = -100.3878238
+    radio = 30000
+    type = 'bar'
+
+r = startpoint()
+
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + str(r.latitude) + ',' + str(r.longitude) + '&radius=' + str(r.radio) + '&type='+ r.type + '&key=' + GMAPS_API_KEY
+response = json.loads(requests.get(url).text)
+lugares = json.dumps(response, indent=4, sort_keys=True)
+
+
+while True:
+    time.sleep(5)
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=' + str(response["next_page_token"]) + '&key=' + GMAPS_API_KEY
+    response = json.loads(requests.get(url).text)
+    try:
+        if response["next_page_token"]:
+            lugares += json.dumps(response, indent=4, sort_keys=True)
+    except KeyError:
+        lugares += json.dumps(response, indent=4, sort_keys=True)
+        print("Bye")
+        break
+
+
+f = open("lugaresAle.txt", "w+")
+f.write(lugares)
+f.close()
+
+
+
+
+
+
