c1092eef7f2c07b026d578e19c333b5e8aef9486
RamonRomeroTec <A01700318@itesm.mx>
2018-05-12 02:02:55 -0500

    Modelos Lugar y Tipo

--- busqueda/models.py
+++ busqueda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Lugar(models.Model):
+    tipo = models.ManyToManyField(TipoLugar)
+    nombre = models.CharField(max_length=100, verbose_name='Nombre del Lugar')
+    id_google = models.CharField(max_length=20, verbose_name="ID Google", default=None )
+    id_facebook = models.CharField(max_length=20, verbose_name="ID Facebook", default=None )
+    id_yelp = models.CharField(max_length=20, verbose_name="ID Yelp", default=None )
+    id_foursquare = models.CharField(max_length=20, verbose_name="ID FourSquare", default=None )
+    calificacion = models.FloatField()
+
+
+class TipoLugar(models.Model):
+    descripcion = models.CharField(max_length=20, verbose_name="Tipo de Lugar")
