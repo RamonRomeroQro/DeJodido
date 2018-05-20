5cde7bf7298fa8724cf77cccef3eb2eef1462c24
RamonRomeroTec <A01700318@itesm.mx>
2018-05-19 21:52:31 -0500

    listado

--- deajodido/settings.py
+++ deajodido/settings.py
@@ -133,7 +133,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = '/static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
