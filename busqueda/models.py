aaa52f1c44d66e1de94de456488e3e56bb56b79b
RamonRomeroTec <A01700318@itesm.mx>
2018-05-19 19:57:07 -0500

    initial parsing to models

--- busqueda/models.py
+++ busqueda/models.py
@@ -23,9 +23,10 @@ class Lugar(models.Model):
     nombre = models.CharField(max_length=100, verbose_name='Nombre del Lugar')
     lat = models.FloatField( verbose_name='Latitud')
     lng = models.FloatField(verbose_name='Longitud')
-    id_google = models.CharField(max_length=20, verbose_name="ID Google", default=None )
+    direccion = models.CharField(max_length=300, verbose_name='Dirección')
+    place_id = models.CharField(max_length=20, verbose_name="ID Google", default=None )
     id_facebook = models.CharField(max_length=20, verbose_name="ID Facebook", default=None )
     id_yelp = models.CharField(max_length=20, verbose_name="ID Yelp", default=None )
     id_foursquare = models.CharField(max_length=20, verbose_name="ID FourSquare", default=None )
-    calificacion = models.FloatField()
+    rating = models.FloatField()
 
