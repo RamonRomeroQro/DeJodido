aae9b8825e5b66dddc6c0c9c2f08fa979fce3673
RamonRomeroTec <A01700318@itesm.mx>
2018-05-23 14:01:54 -0500

    modelos y aplicacion para lugares

--- lugares/admin.py
+++ lugares/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Tags, Estado, Ciudad, Pais, Lugar
+
+
+admin.site.register(Tags)
+admin.site.register(Estado)
+admin.site.register(Ciudad)
+admin.site.register(Pais)
+admin.site.register(Lugar)
