435ac39992eb663624e6007be6c336655b7d2313
RamonRomeroTec <A01700318@itesm.mx>
2018-05-24 12:12:52 -0500

    modelos y aplicacion para lugares

---
+++ python/ramon/yelp.py
@@ -0,0 +1,25 @@
+
+#curl -X GET -H "Authorization: Bearer CpMKG2Z-E_WAiDNoHTnGCUzZ2wUCTPsOwd2B6egscNf6p7BNOY81zt6JjtScUNRaQqq6ttHEqEYLW5PySrEVMe9KFpyeNY9q0Ao7U3ujCGbObr2IDRXTFUD-D1gEW3Yx"  "https://api.yelp.com/v3/businesses/search?term=alquimia&latitude=20.5732807&longitude=-100.3614291"
+
+
+
+
+
+import requests, json, pprint
+
+    #pprint.pprint(response)
+
+
+
+
+lat='20.5924074'
+lng='-100.3788854'
+kyword='alquimia'
+
+busquedaYelp(kyword,lat,lng)
+
+
+
+#Authorization: Bearer <YOUR API KEY>
+
+
