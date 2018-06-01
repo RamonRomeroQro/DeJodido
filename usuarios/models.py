e266548b32863b7cb96314744c8d580edf9ea7e6
Ale Lopez <a01208598@itesm.mx>
2018-05-31 23:56:44 -0500

    -crear el modelo Usuarios -agregar Pillow al requirements.txt

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from lugares.models import Ciudad
 
 # Create your models here.
 
@@ -15,3 +16,5 @@ class Usuario(models.Model):
     fecha_nacimiento = models.DateField(default='', verbose_name='Fecha de Nacimiento')
     genero = models.IntegerField(choices=GENEROS,verbose_name='Genero', null=False, blank=False)
     imagen = models.ImageField(upload_to='imagen_usuarios', verbose_name='Perfil', null=True, blank=True)
+    ciudad = models.ForeignKey(Ciudad, on_delete = models.CASCADE)
+    fb_id = models.CharField(max_length=17, unique=True, blank=True, null=True)
