2525b23bd98da9adce5f230a5819e31aa33c5248
RamonRomeroTec <A01700318@itesm.mx>
2018-06-12 15:04:15 -0500

    ui-design accounts logo

--- landing/views.py
+++ landing/views.py
@@ -9,29 +9,9 @@ from django.http import JsonResponse
 
 #Visualizar partidos en la landing page
 def landing(request):
-    if request.method=='POST':
-        fullname= request.POST['city']
-        parsedlocation= fullname.split(', ')
-        city=parsedlocation[0]
-        state=parsedlocation[1]
-        country=parsedlocation[2]
-        qpais=Pais.objects.get(nombre=country)
-        qstate=Estado.objects.filter(pais=qpais).get(nombre=state)
-        qcity=Ciudad.objects.filter(estado=qstate).get(nombre=city)
-        lugares = Lugar.objects.filter(ciudad=qcity)
-        fotos=[]
+    return render(request, 'landing/index.html')
 
 
-        for l in lugares:
-            images = Imagen.objects.filter(lugar=l)
-            for i in images:
-                fotos.append(i)
-        #pprint.pprint(fotos)
-
-
-        return render(request, 'lugares/list.html', { 'fotos':fotos , 'lugares': lugares })
-    else:
-        return render(request, 'landing/index.html')
 
 
 
