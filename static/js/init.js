a25d89903b33989590092adbc6bf9920031275b0
RamonRomeroTec <A01700318@itesm.mx>
2018-06-12 15:23:23 -0500

    FINAL MERGE 1

--- static/js/init.js
+++ static/js/init.js
@@ -65,3 +65,56 @@ var nav = null;
                 }
               });
   });
+
+    var nav = null;
+    function requestPosition() {
+        if (nav == null) {
+            nav = window.navigator;
+        }
+        var geoloc = nav.geolocation;
+        if (geoloc != null) {
+            geoloc.getCurrentPosition(successCallback, errorCallback);
+        }
+    }
+    function successCallback(position) {
+        document.getElementById("latitude").value =
+            position.coords.latitude;
+        document.getElementById("longitude").value =
+            position.coords.longitude;
+    }
+    function errorCallback(error) {
+        var strMessage = "";
+        // Check for known errors
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                strMessage = "Access to your location is turned off. "  +
+                    "Change your settings to turn it back on.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                strMessage = "Data from location services is " +
+                    "currently unavailable.";
+                break;
+            case error.TIMEOUT:
+                strMessage = "Location could not be determined " +
+                    "within a specified timeout period.";
+                break;
+            default:
+                break;
+        }
+        document.getElementById("status").innerHTML = strMessage;
+    }
+  // Or with jQuery
+/// AQUI EL JQUERY AJAX PARA RECUPERAR LOS NOMBRES DE CIUDADES, CHECA: def listaciudades(request) en view.py
+  $(document).ready(function(){
+      $('select').formSelect();
+     $.ajax({
+                url: '/ajax/get_ciudades/',
+                data: {},
+                dataType: 'json',
+                success: function (data) {
+                    $('input.autocomplete').autocomplete({
+                      data : data
+                    });
+                }
+              });
+  });
