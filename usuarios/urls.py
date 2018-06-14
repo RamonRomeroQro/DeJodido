e2c44987d116582b055b8da8ad328f6fc6f1e2b8
RamonRomeroTec <A01700318@itesm.mx>
2018-06-14 03:35:11 -0500

    login and singup

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from .import views
 app_name = 'usuarios'
 
 urlpatterns = [
-    path('', views.Prueba_index, name='Prueba_index'),
+    path('', views.singup, name='singup'),
     path('resena/<str:nombre_lugar>-<int:id_lugar>', views.prueba_resena, name='Prueba_resena'),
     path('verificacion_FB/', views.verificacion_FB, name='nueva_cuentaFB'),
 ]
