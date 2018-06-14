c3739bd3ea62d51bedc9d00ecd89f4b336fc7c86
RamonRomeroTec <A01700318@itesm.mx>
2018-06-14 03:35:11 -0500

    login and singup

--- usuarios/models.py
+++ usuarios/models.py
@@ -8,18 +8,18 @@ from django.utils import timezone
 # Create your models here.
 
 GENEROS = (
-    (1, 'mujer'),
-    (2, 'hombre'),
-    (3, 'Prefiro no decir')
+    (1, 'Mujer'),
+    (2, 'Hombre'),
+    (3, 'Otro')
 )
 
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     fecha_nacimiento = models.DateField(default='', verbose_name='Fecha de Nacimiento', null=False, blank=False)
-    genero = models.IntegerField(choices=GENEROS,verbose_name='Genero', null=False, blank=False)
-    imagen = models.ImageField(upload_to='imagen_usuarios', verbose_name='Perfil', null=True, blank=True)
-    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    genero = models.IntegerField(choices=GENEROS,verbose_name='Género', null=False, blank=False)
+    imagen = models.ImageField(upload_to='imagen_usuarios', verbose_name='Imágen de Perfil', null=True, blank=True)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE, verbose_name="Ciudad de Residencia")
     fb_id = models.CharField(max_length=20, unique=True, blank=True, null=True)
     resena = models.ManyToManyField(Lugar, through='UsuarioResenaLugar')
 
