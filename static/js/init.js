37759172e6a62a360c4e3e66cda81a6047cc28c8
RamonRomeroTec <A01700318@itesm.mx>
2018-06-15 00:39:34 -0500

    fixing create

--- static/js/init.js
+++ static/js/init.js
@@ -126,11 +126,6 @@ var nav = null;
   });
 
 
-    document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, options);
-  });
-
   // Or with jQuery
 
   $(document).ready(function(){
