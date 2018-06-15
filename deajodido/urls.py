d40c5964e71b170c83f12b2319349dee4e3459a0
RamonRomeroTec <A01700318@itesm.mx>
2018-06-15 17:36:42 -0500

    ok

--- deajodido/urls.py
+++ deajodido/urls.py
@@ -21,6 +21,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
@@ -29,3 +32,6 @@ urlpatterns = [
     path('', include('landing.urls')),
     path('', include('lugares.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
