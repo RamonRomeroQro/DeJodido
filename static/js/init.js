5eb9bc4c61199a05c524abb13f87f09c73a14958
RamonRomeroTec <A01700318@itesm.mx>
2018-06-27 00:36:43 -0500

    ok

--- static/js/init.js
+++ static/js/init.js
@@ -1,12 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
 
 
 var nav = null;
