4b0c4d86ed04c121dda0dd7e6fbbaf155fcc816a
Ale Lopez <a01208598@itesm.mx>
2018-07-04 11:29:23 -0500

    crear contrasenia cuando usuario se conecta con FB

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.singup, name='singup'),
     path('resena/<str:nombre_lugar>-<int:id_lugar>', views.resena, name='resena'),
     path('prueba_fb', views.prueba_fb, name='prueba_fb'),
+    path('fb_extras', views.fb_extras, name='fb_extras'),
 ]
