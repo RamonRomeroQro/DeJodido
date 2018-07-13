754be2c5442edded730f5e6ba873e27673bc16ec
RamonRomeroTec <A01700318@itesm.mx>
2018-07-13 00:36:33 -0500

    Merge remote-tracking branch 'remotes/origin/develop'
    # Conflicts:
    #	deajodido/urls.py

--- deajodido/settings/production.py
+++ deajodido/settings/production.py
@@ -1,3 +1,4 @@
+
 from deajodido.settings.common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -6,43 +7,6 @@ with open('/etc/secret_key.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deajodido',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Google's Keys
-GMAPS_API_KEY = 'AIzaSyC9nWuYkCMxKm_PFZ6m04gCAuz-SeuCE2g'
-GMAPS_API_KEY_JS= GMAPS_API_KEY
-
-#SocialAUTH's Keys
-SOCIAL_AUTH_FACEBOOK_KEY = '805766712952383'  # App ID PRUEBA
-SOCIAL_AUTH_FACEBOOK_SECRET = 'd7b13e308d24be4726f0c81721259a97'  # App Secret PRUEBA
-SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
-
-'''
-from deajodido.settings.common import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['138.197.223.47', 'dejodido.com']
 
@@ -88,4 +52,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
-'''
+
