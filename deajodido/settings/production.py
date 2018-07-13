88f35a4c2760ccaf5b95ce3096be3e35df65de72
RamonRomeroTec <A01700318@itesm.mx>
2018-07-13 00:32:55 -0500

    Merge remote-tracking branch 'remotes/origin/develop'
    # Conflicts:
    #	deajodido/urls.py

--- deajodido/settings/production.py
+++ deajodido/settings/production.py
@@ -1,10 +1,11 @@
 from deajodido.settings.common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-3eh!c!w_&@pn))c^nrmq*cy(#48orq%+4sj#vw6l_z9b&obu&'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
