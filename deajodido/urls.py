5ef2b9a6a0c067ea190aa41a9bcd81cfefe33d0c
RamonRomeroTec <A01700318@itesm.mx>
2018-07-12 20:20:15 -0500

    Merge remote-tracking branch 'remotes/origin/develop'
    # Conflicts:
    #	deajodido/urls.py

--- deajodido/urls.py
+++ deajodido/urls.py
@@ -21,6 +21,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+
+
 urlpatterns = [
 
     path('', include('django.contrib.auth.urls')),
