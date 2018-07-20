f4305c9a0ac953410403801913a2ab8f7569950e
RamonRomeroTec <A01700318@itesm.mx>
2018-07-19 21:19:01 -0500

    NO password login fa cebook

--- sm/urls.py
+++ sm/urls.py
@@ -17,6 +17,10 @@ urlpatterns = [
     path('lugar/<int:id_lugar>', views.detalle_lugar, name='detalle_lugar'),
     path('update_place/<int:id_lugar>', views.update_place, name='update_place'),
     path('update_image/<int:id_image>', views.update_image, name='update_image'),
+    path('quickimages', views.quickimages, name='quickimages'),
+    path('atrue/<int:id_imagen>', views.atrue, name='atrue'),
+    path('afalse/<int:id_imagen>', views.afalse, name='afalse'),
+    path('imagenes', views.imagenes, name='imagenes'),
     path('', views.consola, name='consola'),
 
     #path('listaciudades', views.listaciudades, name='listaciudades'),
