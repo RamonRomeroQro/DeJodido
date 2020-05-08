c30d177336b38a377958db8d4baac04be8110cb2
ramonromeroqro <rr100@live.com.mx>
2020-05-08 13:57:26 -0500

    add st

--- deajodido/settings.py
+++ deajodido/settings.py
@@ -9,9 +9,7 @@ TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
