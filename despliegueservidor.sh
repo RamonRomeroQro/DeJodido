86aac3c35b60678f91f1bde8a55a1e2bfe46f3ae
Ramón Romero <rr100@live.com.mx>
2020-05-14 21:06:38 -0500

    fx deploy

--- despliegueservidor.sh
+++ despliegueservidor.sh
@@ -1,8 +1,6 @@
 
 #!/bin/bash
-cd /home/jodido/
 source venv/bin/activate
-cd NewJodido/
 git pull origin master
 pip install -r requirements.txt
 python manage.py makemigrations landing
@@ -10,8 +8,6 @@ python manage.py migrate
 python manage.py makemigrations lugares
 python manage.py migrate
 python manage.py makemigrations usuarios
-python manage.py migrate
-python manage.py makemigrations eastereggs
 python manage.py migrate
 python manage.py makemigrations sm
 python manage.py migrate
